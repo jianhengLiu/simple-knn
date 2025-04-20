83e907c6fdc33752be0bdd0e906e7fc5968d4109
Chris Liu <943678231@qq.com>
2025-04-20 21:04:57 +0800

    Add LICENSE and README files; update spatial.h include

--- spatial.h
+++ spatial.h
@@ -3,12 +3,12 @@
  * GRAPHDECO research group, https://team.inria.fr/graphdeco
  * All rights reserved.
  *
- * This software is free for non-commercial, research and evaluation use 
+ * This software is free for non-commercial, research and evaluation use
  * under the terms of the LICENSE.md file.
  *
  * For inquiries contact  george.drettakis@inria.fr
  */
 
-#include <torch/extension.h>
+#include <torch/torch.h>
 
-torch::Tensor distCUDA2(const torch::Tensor& points);
+torch::Tensor distCUDA2(const torch::Tensor &points);
